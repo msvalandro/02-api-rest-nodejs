44f6a74b4a32fd1ea6a176a5afede244aee8c4a2
Matheus Valandro <msvalandro@proton.me>
2025-03-10 19:45:17 +0000

    feat: implement POST endpoint for creating transactions
    - Add POST route to create new transactions
    - Implement input validation using Zod schema
    - Insert transaction data into the database
    - Set appropriate status code for successful creation
    - Update route registration with 'transactions' prefix

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,28 @@
+import { randomUUID } from 'node:crypto'
+
 import { FastifyInstance } from 'fastify'
-import knex from 'knex'
+import { z } from 'zod'
+
+import { knex } from '../database'
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const tables = knex('sqlite_schema').select('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return tables
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
