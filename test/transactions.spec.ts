960d286f97627d0bc5f0b2f39aa7bf98cbae96e3
Matheus Valandro <msvalandro@proton.me>
2025-03-11 20:40:53 +0000

    test: add test for listing all transactions
    - Add new test case to verify the ability to list all transactions
    - Include assertions to check the response body and status code
    - Utilize cookies from transaction creation for authentication in list request

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import { afterAll, beforeAll, describe, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 import { app } from '../src/app'
 
@@ -17,5 +17,25 @@ describe('Routes: Transactions', () => {
       .post('/transactions')
       .send({ title: 'New transaction', amount: 5000, type: 'credit' })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({ title: 'New transaction', amount: 5000, type: 'credit' })
+
+    const cookies = createTransactionResponse.get('Set-Cookie') ?? []
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
